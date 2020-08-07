c5d105b0a63cc79d81cf8a38e5f70f572b42f129
ArinaHodanova <69355804+ArinaHodanova@users.noreply.github.com>
2020-08-07 17:40:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var instance = new vidbg(".video", {
+  mp4: "video/world.mp4", 
+  webm: "video/world.webm",
+  poster: "video/poster.jpg",
+  overlay: false,
+});
+
+var rellax = new Rellax(".rocket");
+if (document.body.clientWidth < 567) {
+	rellax.destroy();
+}
+
+
+ AOS.init();
